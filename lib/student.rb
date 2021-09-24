aa3f7f1c8b3a2dd5a259cc4941e65e2b7e13eae9
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-09-24 12:50:25 -0500

    commit tree 4567af0cb48f3d37102c44b0939d62328f4f8bcd

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+    #attr_reader :knowledge
 
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(string_o_knowing)
+        @knowledge << string_o_knowing
+    end
+    
+#attr_reader below
+    def knowledge
+        @knowledge
+    end
 end
